3722fcb3ff3f867b0f8b4dce6604ebe29b1f629b
WounderDragon <46166716+WounderDragon@users.noreply.github.com>
2021-02-26 11:51:58 +0100

    Working code for olx websites
    - Working scrapper for OLX websites
    - Calculate average price for search item
    - Prepare code for other popular buy website

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.tabs.getSelected(null, function(tab) {
-    tab = tab.id;
-    tabUrl = tab.url;
-
-    console.log(tabUrl);
-});
--- js.js
+++ js.js
@@ -1,7 +1,21 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.action == "getSource") {
-        //Here print body of active tab
-      message.innerText = request.source;
+      var array_price = []
+      var doc = new DOMParser().parseFromString(request.source, "text/html")
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+        if(url.includes("olx")){           
+            //Code for olx website:
+            var length_elements =doc.body.querySelectorAll('p.price').length;
+            for (var i = 0; i <length_elements;i++){
+                var single_element =doc.body.querySelectorAll('p.price')[i].querySelector('strong').innerHTML.toString().replace(/\s/g,'');
+                var matches = single_element.match(/(\d+)/)[0];
+                array_price.push(parseInt(matches));
+            }
+        message.innerText = (((array_price.reduce((a, b) => a + b, 0))/length_elements).toFixed(2)+" PLN");//Average price for search item
+        }       
+    });
+
     }
   });
   
@@ -10,7 +24,6 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     chrome.tabs.executeScript(null, {
       file: "getPagesSource.js"
     }, function() {
-      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
       if (chrome.runtime.lastError) {
         message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
       }
